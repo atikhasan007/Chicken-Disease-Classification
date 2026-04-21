894d5c78d5368817e6905a7e8878c6c51aadc38a
Md Atik Hasan <atikhasan007@gmail.com>
2026-04-21 10:46:48 +0600

    model trainer 03 added

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from src.cnnClassifier import logger
 # from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.cnnClassifier.pipeline.stage_02_prepare_base_model import PrparBaseModelTrainingPipeline
+from src.cnnClassifier.pipeline.stage_03_model_trainer import ModelTrainingPipeline
+
+
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -21,6 +24,19 @@ STAGE_NAME = "Prepare base model"
 try:
     logger.info(f">>>> stage {STAGE_NAME} started <<<<")
     data_ingestion = PrparBaseModelTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>> stage{STAGE_NAME} completed <<<<<\n\nx=======")
+
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+
+STAGE_NAME = "Trainer"
+try:
+    logger.info(f">>>> stage {STAGE_NAME} started <<<<")
+    data_ingestion = ModelTrainingPipeline()
     data_ingestion.main()
     logger.info(f">>>>> stage{STAGE_NAME} completed <<<<<\n\nx=======")
 
