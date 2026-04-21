6a9c4a94171427d382ded0e881bdc895c7395c24
Md Atik Hasan <atikhasan007@gmail.com>
2026-04-21 09:55:07 +0600

    base_model_setup 02

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from src.cnnClassifier import logger
 
 # from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-
+from src.cnnClassifier.pipeline.stage_02_prepare_base_model import PrparBaseModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -17,3 +17,13 @@ except Exception as e:
     raise e
 
 
+STAGE_NAME = "Prepare base model"
+try:
+    logger.info(f">>>> stage {STAGE_NAME} started <<<<")
+    data_ingestion = PrparBaseModelTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>> stage{STAGE_NAME} completed <<<<<\n\nx=======")
+
+except Exception as e:
+    logger.exception(e)
+    raise e
